c19b728e4d7c86e67f21549ef8d02e55997241c1
Victor Mwiti <vicmwiti@gmail.com>
2023-05-01 09:12:16 -0700

    fixed tab error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #Executes a bash command
 exec { 'kill':
-	command	=> 'pkill -f killmenow',
-	path	=> ['usr/bin', '/usr/sbin']
+	command  => 'pkill -f killmenow',
+	path  => ['usr/bin', '/usr/sbin']
 }
