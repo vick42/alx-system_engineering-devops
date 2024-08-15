e5cf826e2373042fd196a80509fcec94691c36c1
vick42 <vicmwiti@gmail.com>
2024-08-15 01:05:41 -0700

    reviewed spacing errors

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #For this project I utilised strace followed by Puppet to automate th patch for 500 error on apache
 
 exec { 'fix_typo':
-	path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+	path  => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
 	command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-	provider => 'shell'}
+	provider  => 'shell'}
